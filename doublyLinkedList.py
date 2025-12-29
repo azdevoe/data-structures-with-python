486ec50fab0245378ac4c7efe27f69932d9449c4
devoe <aroabdulazeez@gmail.com>
2025-12-29 18:42:37 +0000

    more changes

--- doublyLinkedList.py
+++ doublyLinkedList.py
@@ -15,7 +15,6 @@ class Logic:
         if self.head == None:
             self.head = newNode
             self.tail = newNode
-            return
         else:
             tailP = self.tail
             newNode.prev = tailP
@@ -24,9 +23,27 @@ class Logic:
     
     def addHead(self,value):
         newNode = doubly(value)
-        temp = self.head
-        newNode.next= temp
-        self.head=newNode
+        if self.head==None:
+            self.head=newNode
+            self.tail=newNode
+        else:
+            temp = self.head
+            newNode.next= temp
+            self.head=newNode
+        
+    def pop(self):
+        if self.head == None:
+            return False
+        else:
+            temp = self.tail.prev
+            temp.next = None
+            self.tail.prev =None
+            self.tail = temp
+    def removeHead(self):
+        temp=self.head.next
+        temp.prev = None
+        self.head.next=None
+        self.head=temp
     def display(self):
         arr= []
         temp=self.head
@@ -35,7 +52,11 @@ class Logic:
             temp=temp.next
         return arr
 v1 = Logic()
+v1.addHead(1)
+v1.addHead(2)
 v1.push(4)
 v1.push(5)
 v1.addHead(1)
+v1.pop()
+v1.removeHead()
 print(v1.display())
