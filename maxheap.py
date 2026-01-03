6e3ad276746c263573d1f2ee1e8741e0352e81c6
devoe <aroabdulazeez@gmail.com>
2026-01-03 11:27:04 +0000

    just finished it

--- maxheap.py
+++ maxheap.py
@@ -16,7 +16,68 @@ class maxHeap:
         return self.arr[self.getRightIndex(index)]
     def hasLeftChild(self,index):
         return self.getLeftIndex(index)<len(self.arr)
-    def hasRightChild(self):
-        return
-    def hasParent(self):
-        return
+    def hasRightChild(self,index):
+        return self.getRightIndex(index)<len(self.arr)
+    def hasParent(self,index):
+        return self.getParentIndex(index)>=0
+    
+    def displayArr(self):
+        return self.arr
+    def swap(self,first,second):
+        [self.arr[first],self.arr[second]]=[self.arr[second],self.arr[first]]
+    
+    def peak(self):
+        if len(self.arr) == 0:
+            return 'empty'
+        return self.arr[0]
+    
+    def add(self,value):
+        self.arr.append(value)
+        self.heapifyUp()
+    
+    def removeTop(self):
+        if len(self.arr) == 1:
+            self.arr.pop()
+            return
+        [self.arr[0],self.arr[len(self.arr)-1]]=[self.arr[len(self.arr)-1],self.arr[0]]
+        self.arr.pop()
+        self.heapifyDown()
+        
+    def biggerChild(self,idx):
+        if self.hasLeftChild(idx) and self.hasRightChild(idx):
+            if self.getLeftChild(idx)>self.getRightChild(idx):
+                return self.getLeftIndex(idx)
+            else:
+                return self.getRightIndex(idx)
+        else:
+            return self.getLeftIndex(idx)
+    def heapifyUp(self):
+        if len(self.arr) ==0:
+            return 'empty'
+        idx=len(self.arr)-1
+        while self.hasParent(idx) and self.getParent(idx)<self.arr[idx]:
+            self.swap(idx,self.getParentIndex(idx))
+            idx=self.getParentIndex(idx)
+
+    def heapifyDown(self):
+        idx=0
+       
+        while self.hasLeftChild(idx):
+            bigger=self.biggerChild(idx)
+            if self.arr[idx]<self.arr[bigger]:
+                self.swap(idx,bigger)
+                idx=bigger
+            else:
+                break
+mh=maxHeap()
+mh.add(2)
+mh.add(1)
+mh.add(5)
+mh.add(17)
+mh.add(22)
+mh.add(66)
+mh.removeTop()
+mh.removeTop()
+mh.removeTop()
+print(mh.peak())
+print(mh.displayArr())
