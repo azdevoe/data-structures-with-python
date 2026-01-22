fcc659990df5d9145140ad771181f00520b373b6
devoe <aroabdulazeez@gmail.com>
2026-01-22 08:29:27 +0000

    22/1/26

--- recurr/fixedsliding.py
+++ recurr/fixedsliding.py
@@ -2,6 +2,6 @@ def sliding(arr,sub):
     final=curr=sum(arr[:sub])
     for end in range(sub,len(arr)):
         curr=curr+arr[end]-arr[end-sub]
-        final = max(final,curr)
+        final=max(final,curr)
     return final
 print(sliding([3,4,1,77,8,4],3))
--- recurr/kadane.py
+++ recurr/kadane.py
@@ -2,8 +2,8 @@ def kadane(arr):
     final=arr[0]
     curr=0
     for num in arr:
-        curr = max(curr,0)
+        curr=max(curr,0)
         curr+=num
-        final = max(final,curr)
+        final=max(final,curr)
     return final
 print(kadane([9,2,4,-50,8]))
