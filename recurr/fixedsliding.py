caa1b0fee678a4a3a314988680936dd7cc2ae1ac
devoe <aroabdulazeez@gmail.com>
2026-02-11 19:22:13 +0000

    daily dsa

--- recurr/fixedsliding.py
+++ recurr/fixedsliding.py
@@ -1,7 +1,7 @@
 def sliding(arr,sub):
     final=curr=sum(arr[:sub])
     for i in range(sub,len(arr)):
-        curr+=arr[i]-arr[i-sub]
-        final = max(final,curr)
+        curr=curr+arr[i]-arr[i-sub]
+        final=max(final,curr)
     return final
-print(sliding([3,4,1,77,8,4],3))
+print(sliding([3,4,1,77,-100,80,8,4],3))
--- recurr/prefixSum.py
+++ recurr/prefixSum.py
@@ -1,8 +1,8 @@
 class PrefixSum:
     def __init__(self,arr):
-        self.p = [0]*len(arr)
+        self.p= [0]*len(arr)
         self.p[0]=arr[0]
-        for i in range(1,len(arr)):
+        for i in range(1,len(self.p)):
             self.p[i]=self.p[i-1]+arr[i]
     def ps(self,l,r):
         if l==0: return self.p[r]
